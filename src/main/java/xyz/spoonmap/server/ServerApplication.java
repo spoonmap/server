84bdc08469822c96941197ac581079b80c83aa2e
Dongyeol <eastheat10@gmail.com>
2023-03-21 17:09:50 +0900

    feat: health check for test

--- src/main/java/xyz/spoonmap/server/ServerApplication.java
+++ src/main/java/xyz/spoonmap/server/ServerApplication.java
@@ -2,6 +2,10 @@ package xyz.spoonmap.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +14,13 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+}
+
+@RestController
+class HealthCheck {
+	@GetMapping("health-check")
+	@ResponseStatus(HttpStatus.OK)
+	public String healthCheck() {
+		return "OK";
+	}
 }
