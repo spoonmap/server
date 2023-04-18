bfe2d9332bbfa6e0345cad658da8d9482d98aedb
Dongyeol <eastheat10@gmail.com>
2023-04-18 20:05:26 +0900

    refactor: AOP, EventListener 역할 분리 #60
    close

--- src/main/java/xyz/spoonmap/server/notification/aop/NotificationAspect.java
+++ src/main/java/xyz/spoonmap/server/notification/aop/NotificationAspect.java
@@ -1,15 +1,11 @@
 package xyz.spoonmap.server.notification.aop;
 
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import xyz.spoonmap.server.comment.dto.response.CommentResponseDto;
-import xyz.spoonmap.server.exception.domain.member.MemberNotFoundException;
-import xyz.spoonmap.server.member.entity.Member;
-import xyz.spoonmap.server.member.repository.MemberRepository;
 import xyz.spoonmap.server.notification.entity.enums.NotificationType;
 import xyz.spoonmap.server.notification.event.NotificationEvent;
 import xyz.spoonmap.server.relation.dto.response.FollowAddResponse;
@@ -25,14 +21,16 @@ public class NotificationAspect {
     public void addCommentNotification(CommentResponseDto commentResponse) {
 
         applicationEventPublisher
-            .publishEvent(new NotificationEvent(commentResponse.authorId(), commentResponse.id(), NotificationType.COMMENT));
+            .publishEvent(
+                new NotificationEvent(commentResponse.authorId(), commentResponse.id(), NotificationType.COMMENT));
     }
 
     @AfterReturning(value = "execution(* xyz.spoonmap.server.relation.service.*.requestFollow(..))", returning = "followResponse")
     public void addFollowNotification(FollowAddResponse followResponse) {
 
         applicationEventPublisher
-            .publishEvent(new NotificationEvent(followResponse.receiverId(), followResponse.senderId(), NotificationType.FOLLOW));
+            .publishEvent(
+                new NotificationEvent(followResponse.receiverId(), followResponse.senderId(), NotificationType.FOLLOW));
     }
 
 }
