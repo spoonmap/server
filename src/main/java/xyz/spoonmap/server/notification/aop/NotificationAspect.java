413942b56f4cd9b2585cc15810272df663dce735
Dongyeol <eastheat10@gmail.com>
2023-04-18 18:53:07 +0900

    refactor: 댓글 알림 수정 #60

--- src/main/java/xyz/spoonmap/server/notification/aop/NotificationAspect.java
+++ src/main/java/xyz/spoonmap/server/notification/aop/NotificationAspect.java
@@ -9,7 +9,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import xyz.spoonmap.server.authentication.CustomUserDetail;
 import xyz.spoonmap.server.comment.dto.response.CommentResponseDto;
+import xyz.spoonmap.server.exception.domain.member.MemberNotFoundException;
 import xyz.spoonmap.server.member.entity.Member;
+import xyz.spoonmap.server.member.repository.MemberRepository;
 import xyz.spoonmap.server.notification.entity.enums.NotificationType;
 import xyz.spoonmap.server.notification.event.NotificationEvent;
 import xyz.spoonmap.server.relation.dto.response.FollowAddResponse;
@@ -20,15 +22,20 @@ import xyz.spoonmap.server.relation.dto.response.FollowAddResponse;
 public class NotificationAspect {
 
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final MemberRepository memberRepository;
 
     @AfterReturning(value = "execution(* xyz.spoonmap.server.comment.service.*.create(..))", returning = "returnValue")
     public void addCommentNotification(CommentResponseDto returnValue) {
-        Member member = this.getMember();
-        if (Objects.equals(member.getId(), returnValue.authorId())) {
+        Member commentWriter = this.getMember();
+        Member author = memberRepository.findById(returnValue.authorId())
+                                        .orElseThrow(MemberNotFoundException::new);
+
+        if (Objects.equals(commentWriter.getId(), author.getId())) {
             return;
         }
+
         applicationEventPublisher
-            .publishEvent(new NotificationEvent(member, NotificationType.COMMENT, returnValue.id()));
+            .publishEvent(new NotificationEvent(author, NotificationType.COMMENT, commentWriter.getId()));
     }
 
     @AfterReturning(value = "execution(* xyz.spoonmap.server.relation.service.*.requestFollow(..))", returning = "returnValue")
--- src/main/java/xyz/spoonmap/server/notification/event/eventlistener/NotificationEventListener.java
+++ src/main/java/xyz/spoonmap/server/notification/event/eventlistener/NotificationEventListener.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import xyz.spoonmap.server.member.repository.MemberRepository;
 import xyz.spoonmap.server.notification.entity.Notification;
 import xyz.spoonmap.server.notification.event.NotificationEvent;
 import xyz.spoonmap.server.notification.repository.NotificationRepository;
@@ -14,6 +15,7 @@ import xyz.spoonmap.server.notification.repository.NotificationRepository;
 public class NotificationEventListener {
 
     private final NotificationRepository notificationRepository;
+    private final MemberRepository memberRepository;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Async
